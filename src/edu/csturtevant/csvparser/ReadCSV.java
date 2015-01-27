a6b94c0bbe849c40b518420d57dc07bf62326d17
Christopher Sturtevant <chris@xris.us>
2015-01-27 00:42:22 -0500

    Added a Package, Renamed the file
    I added the CSV parser to a package named edu.csturtevant.csvparser, and refactored the name from ReadCVS to ReadCSV.
    I also added a large block comment containing contact information, a description of the program and a list of the user's responsibilities.
    
    [ticket: 0.0.3]
    
    [Date: 01.26.2015]

--- src/edu/csturtevant/csvparser/ReadCSV.java
+++ src/edu/csturtevant/csvparser/ReadCSV.java
@@ -1,17 +1,28 @@
+/* Christopher Sturtevant 1.26.15 csturtevant@rollins.edu
+ * Program ReadCSV
+ * This is a program that has been created to output the contents of
+ * a csv to a new file, reducing, and cleaning the csv of duplicates,
+ * and unnecessary data. This program is menu driven, and executes
+ * with a switch statement.
+ *
+ * User Responsibility:
+ * The user needs to supply the path of the file. //Possible change to a window pane prompt?
+ * The user also needs to drive the menu and choose the paths he or she would like to take.
+*/
 
-
+package edu.csturtevant.csvparser;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class ReadCVS {
+public class ReadCSV {
 
 
     public static void main(String[] args) {
 
-        ReadCVS obj = new ReadCVS();
+        ReadCSV obj = new ReadCSV();
         obj.run();
 
     }
