aa3ccaa29614f73d5e58281f57b1a7b30c352e5c
Christopher Sturtevant <chris@xris.us>
2015-02-05 11:43:20 -0500

    Working CSVCodingPractice.java
    Working ArrrayList of ArrayList
    Record Finding:
    mI = clients.get(1); //Row 1
    mI.get(2); //Column 2
    
    [ticket: 0.0.13]
    
    [Date: 02.05.2015]

--- src/edu/csturtevant/csvparser/CodingCSVPractice.java
+++ src/edu/csturtevant/csvparser/CodingCSVPractice.java
@@ -4,35 +4,80 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by chrissturtevant0 on 2/4/15.
  * This is for practicing using and modifying array values.
  */
-/*
 
 public class CodingCSVPractice {
-    private String[] mI = null; //Magazine Information
+    private ArrayList<ArrayList<String>> clients = new ArrayList<ArrayList<String>>();
+    private ArrayList<String> mI = null;
     public static void main (String[] args) {
+        CodingCSVPractice obj = new CodingCSVPractice();
+        obj.run();
     }
 
+    public void outputClients() {
 
+        for(int i = 0; i < clients.size(); i++) {
+            mI = clients.get(i);
+            for(int j = 0; j < mI.size(); j++) {
+                if( i != 0 && j == 0 )
+                    System.out.print( mI.get(j) + "\t\t" );
+                else if( i == 0 && j == 2 )
+                    System.out.print( mI.get(j) + "\t\t\t" );
+                else if( j == 2 && mI.get(j).length() >= 21 )
+                    System.out.print( mI.get(j) + "\t" );
+                else if( j == 2 && mI.get(j).length() <= 21 && mI.get(j).length() > 10 )
+                    System.out.print( mI.get(j) + "\t\t" );
+                else if( j == 2 && mI.get(j).length() <= 11 )
+                    System.out.print( mI.get(j) + "\t\t\t" );
+                else if( i != 0 && j == 3 )
+                    System.out.print( mI.get(j) + "\t\t" );
+                else if( j == 1 && mI.get(j).length() >= 13 )
+                    System.out.print( mI.get(j) + "\t\t" );
+                else if( j == 1 )
+                    System.out.print( mI.get(j) + "\t\t\t" );
+                else
+                    System.out.print( mI.get(j) + '\t' );
+            }
+            System.out.println( '\n' );
+        }
+        mI = clients.get(1);
+        System.out.println(mI.get(2));
+        System.out.println("Done");
+    }
+
+    /* Right now this is getting the data from the CSV file
+         * and putting it into the collection "clients"
+         */
     public void run() {
 
-        String csvFile = "/Users/chrissturtevant0/Google Drive/Programming/Projects/GitHub/College/Machine Learning/CSVParser/Data/DirtyData1.csv";
+        String csvFile = "/Users/chrissturtevant0/Google Drive/Programming/Projects/GitHub/College/" +
+                         "Machine Learning/CSVParser/Combined_Data_Unclean.csv";
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
+        String[] wordsArray;
 
         try {
 
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
 
-
                 // use comma as separator
-                mI = line.split(cvsSplitBy);
-                clients.add(mI);
+
+                if ( !(line.equals(",,,,")) )
+                {
+                    wordsArray = line.split(cvsSplitBy);
+                    mI = new ArrayList<String>();
+                    for(String each : wordsArray) {
+                        mI.add(each);
+                    }
+                    clients.add(mI);
+                }
 
             }
 
@@ -51,6 +96,6 @@ public class CodingCSVPractice {
         }
         outputClients();
     }
-    }
+
+
 }
-*/
